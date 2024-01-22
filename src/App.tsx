ac53f8326d06414f0fab9241d8acf8f1dcd421af
John Adler <john.adler@protonmail.ch>
2024-01-22 17:21:54 -0500

    Add xycolor template.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import React, {useEffect} from 'react';
-import {getKeplrFromWindow} from "./util/getKeplrFromWindow";
-import {OsmosisChainInfo} from "./constants";
-import {Balances} from "./types/balance";
-import {Dec, DecUtils} from "@keplr-wallet/unit";
-import {sendMsgs} from "./util/sendMsgs";
-import {api} from "./util/api";
-import {simulateMsgs} from "./util/simulateMsgs";
-import {MsgSend} from "./proto-types-gen/src/cosmos/bank/v1beta1/tx";
+import React, { useEffect } from 'react';
+import { getKeplrFromWindow } from "./util/getKeplrFromWindow";
+import { OsmosisChainInfo } from "./constants";
+import { Balances } from "./types/balance";
+import { Dec, DecUtils } from "@keplr-wallet/unit";
+import { sendMsgs } from "./util/sendMsgs";
+import { api } from "./util/api";
+import { simulateMsgs } from "./util/simulateMsgs";
+import { MsgSend } from "./proto-types-gen/src/cosmos/bank/v1beta1/tx";
 import "./styles/container.css";
 import "./styles/button.css";
 import "./styles/item.css";
 
 function App() {
-  const [address, setAddress] = React.useState<string>('');
-  const [balance, setBalance] = React.useState<string>('');
-
-  const [recipient, setRecipient] = React.useState<string>('');
-  const [amount, setAmount] = React.useState<string>('');
+  const [pixel_x, setPixelX] = React.useState<string>('');
+  const [pixel_y, setPixelY] = React.useState<string>('');
+  const [pixel_color, setPixelColor] = React.useState<string>('');
 
   useEffect(() => {
     init();
@@ -25,7 +23,7 @@ function App() {
   const init = async () => {
     const keplr = await getKeplrFromWindow();
 
-    if(keplr) {
+    if (keplr) {
       try {
         await keplr.experimentalSuggestChain(OsmosisChainInfo);
       } catch (e) {
@@ -36,44 +34,18 @@ function App() {
     }
   }
 
-  const getKeyFromKeplr = async () => {
-    const key = await window.keplr?.getKey(OsmosisChainInfo.chainId);
-    if (key) {
-      setAddress(key.bech32Address)
-    }
-  }
-
-  const getBalance = async () => {
-    const key = await window.keplr?.getKey(OsmosisChainInfo.chainId);
-
-    if (key) {
-      const uri = `${OsmosisChainInfo.rest}/cosmos/bank/v1beta1/balances/${key.bech32Address}?pagination.limit=1000`;
-
-      const data = await api<Balances>(uri);
-      const balance = data.balances.find((balance) => balance.denom === "uosmo");
-      const osmoDecimal = OsmosisChainInfo.currencies.find((currency) => currency.coinMinimalDenom === "uosmo")?.coinDecimals;
-
-      if(balance) {
-        const amount = new Dec(balance.amount, osmoDecimal);
-        setBalance(`${amount.toString(osmoDecimal)} OSMO`)
-      } else {
-        setBalance(`0 OSMO`)
-      }
-    }
-  }
-
-  const sendBalance = async () => {
+  const submitPixel = async () => {
     if (window.keplr) {
       const key = await window.keplr.getKey(OsmosisChainInfo.chainId);
       const protoMsgs = {
         typeUrl: "/cosmos.bank.v1beta1.MsgSend",
         value: MsgSend.encode({
           fromAddress: key.bech32Address,
-          toAddress: recipient,
+          toAddress: key.bech32Address,
           amount: [
             {
               denom: "uosmo",
-              amount: DecUtils.getTenExponentN(6).mul(new Dec(amount)).truncate().toString(),
+              amount: DecUtils.getTenExponentN(6).mul(new Dec(0)).truncate().toString(),
             },
           ],
         }).finish(),
@@ -84,19 +56,23 @@ function App() {
           OsmosisChainInfo,
           key.bech32Address,
           [protoMsgs],
-          [{denom: "uosmo",
-            amount: "236",}]
-          );
+          [{
+            denom: "uosmo",
+            amount: "236",
+          }]
+        );
 
-        if(gasUsed) {
+        if (gasUsed) {
           await sendMsgs(
             window.keplr,
             OsmosisChainInfo,
             key.bech32Address,
             [protoMsgs],
             {
-              amount: [{denom: "uosmo",
-                amount: "236",}],
+              amount: [{
+                denom: "uosmo",
+                amount: "236",
+              }],
               gas: Math.floor(gasUsed * 1.5).toString(),
             })
         }
@@ -112,71 +88,47 @@ function App() {
 
   return (
     <div className="root-container">
-        <div style={{
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+        padding: "16px"
+      }}>
+        <h1>🎨 Celestia Paint</h1>
+      </div>
+
+      <div className="item">
+        <div className="item-title">
+          Change Pixel
+        </div>
+
+        <div className="item-content">
+          <div style={{
             display: "flex",
-            justifyContent: "center",
-            padding: "16px"
-        }}>
-          <img src="/keplr-logo.png" style={{maxWidth: "200px"}} alt="keplr-logo" />
+            flexDirection: "column"
+          }}>
+            X:
+            <input type="text" value={pixel_x} onChange={(e) => setPixelX(e.target.value)} />
+          </div>
+
+          <div style={{
+            display: "flex",
+            flexDirection: "column"
+          }}>
+            Y:
+            <input type="text" value={pixel_y} onChange={(e) => setPixelY(e.target.value)} />
+          </div>
+
+          <div style={{
+            display: "flex",
+            flexDirection: "column"
+          }}>
+            Color:
+            <input type="text" value={pixel_color} onChange={(e) => setPixelColor(e.target.value)} />
+          </div>
+
+          <button className="keplr-button" onClick={submitPixel}>Send</button>
         </div>
 
-
-
-      <div className="item-container">
-        <div className="item">
-          <div className="item-title">
-            Get OSMO Address
-          </div>
-
-          <div className="item-content">
-            <div>
-              Address: {address}
-            </div>
-
-            <div>
-              <button className="keplr-button" onClick={getKeyFromKeplr}>Get Address</button>
-            </div>
-          </div>
-        </div>
-
-        <div className="item">
-          <div className="item-title">
-            Get OSMO Balance
-          </div>
-
-          <div className="item-content">
-            Balance: {balance}
-
-            <button className="keplr-button" onClick={getBalance}>Get Balance</button>
-          </div>
-        </div>
-
-        <div className="item">
-          <div className="item-title">
-            Send OSMO
-          </div>
-
-          <div className="item-content">
-            <div style={{
-              display: "flex",
-              flexDirection: "column"
-            }}>
-              Recipient:
-              <input type="text" value={recipient} onChange={(e) => setRecipient(e.target.value)} />
-            </div>
-
-            <div style={{
-              display: "flex",
-              flexDirection: "column"
-            }}>
-              Amount:
-              <input type="text" value={amount} onChange={(e) => setAmount(e.target.value)} />
-            </div>
-
-            <button className="keplr-button" onClick={sendBalance}>Send</button>
-          </div>
-
-        </div>
       </div>
     </div>
   );
